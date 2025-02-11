6e168232bb3c3bbae18e8192441c078296bda24a
jessie5557 <ychen38@chicagobooth.edu>
2025-02-11 08:22:53 +0000

    square results

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -2,4 +2,10 @@ class CalcController < ApplicationController
   def square_new
     render({:template => "calc_templates/square_new"}) 
   end
+
+  def square_results
+    @the_num = params.fetch("user_number").to_f
+    @result = @the_num ** 2
+    render({:template => "calc_templates/square_results"})
+  end
 end
