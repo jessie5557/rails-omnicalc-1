d03effa3489af96a67d89b961bd75080006ab79b
jessie5557 <ychen38@chicagobooth.edu>
2025-02-11 08:37:32 +0000

    square root results

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -8,4 +8,23 @@ class CalcController < ApplicationController
     @result = @the_num ** 2
     render({:template => "calc_templates/square_results"})
   end
+
+
+  def square_root_new
+    render({:template => "calc_templates/square_root_new"})
+  end
+
+  def square_root_results
+    @the_num = params.fetch("user_number").to_f
+    @result = @the_num ** 0.5
+    render({:template => "calc_templates/square_root_results"})
+  end
+
+
+  def payment
+    render({:template => "calc_templates/payment"})
+  
+  end
+
+
 end
